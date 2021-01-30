2a7ca6861acb4b5060fe8b0f7cde52f433cc0510
Levi McClenny <levimcclenny@tamu.edu>
2021-01-30 11:47:57 -0600

    commit from bash

--- tensordiffeq/tensordiffeq/_build/jupyter_execute/notebooks.py
+++ tensordiffeq/tensordiffeq/_build/jupyter_execute/notebooks.py
@@ -1,7 +1,7 @@
 # Content with notebooks
 
 You can also create content with Jupyter Notebooks. This means that you can include
-code blocks and their outputs in your book.
+code blocks and their outputs in your book. This is a change
 
 ## Markdown + notebooks
 
