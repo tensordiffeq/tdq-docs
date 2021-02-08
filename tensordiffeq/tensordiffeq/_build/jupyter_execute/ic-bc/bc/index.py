a45ed57b62a65806101c1927d582513fc8f941bd
Levi McClenny <levimcclenny@tamu.edu>
2021-02-08 15:39:00 -0600

    commit from bash script

--- tensordiffeq/tensordiffeq/_build/jupyter_execute/ic-bc/bc/index.py
+++ tensordiffeq/tensordiffeq/_build/jupyter_execute/ic-bc/bc/index.py
@@ -21,6 +21,18 @@ Args:
 - `domain` - a `domain` object containing the variables in the domain
 - `val` - a `float` containing the value to be enforced at the boundary
 - `var` - a `str` indicating which variable should be enforced by the value in `val`
-- 'target' a `str` indicating whether the value listed in `val` will be targeting the `upper` or `lower` boundary on `var`
+- `target` - a `str` indicating whether the value listed in `val` will be targeting the `upper` or `lower` boundary on `var`
 
 to create a simple dirichletBC, one could define upper and lower boundary values on the `x` value from the [IC definition section](../ic/index.ipynb)
+
+```{code-block} python
+upper_x = dirichlectBC(Domain, val=0.0, var='x', target="upper")
+lower_x = dirichlectBC(Domain, val=0.0, var='x', target="lower")
+```
+
+This will force the upper and lower boundary value of 0.0 on the upper and lower boundaries of `x` for a 1D spatio-temporal problem.
+
+```{note}
+Currently TensorDiffEq doesn't support functions as Dirichlet BCs. In the near future this will be a feature and will contain a similar
+interface as the [IC function definition](../ic/index.ipynb). Bear with us as we add features to help you solve your problems!
+```
