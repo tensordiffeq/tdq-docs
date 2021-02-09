e6741c957f5bf9d5004a97a4be7b1a71c00f8b7b
Levi McClenny <levimcclenny@tamu.edu>
2021-02-09 12:00:03 -0600

    commit from bash script

--- tensordiffeq/tensordiffeq/_build/jupyter_execute/model/compiling/index.py
+++ tensordiffeq/tensordiffeq/_build/jupyter_execute/model/compiling/index.py
@@ -3,3 +3,4 @@
 Once you've defined your problem, it must be compiled such that TensorDiffEq can build the loss function described
 by the boundary conditions, initial conditions, and physics defined in the previous sections.
 
+Additionally,
