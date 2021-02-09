9d8a754d80a2c95644feb2e2b691817123df3168
Levi McClenny <levimcclenny@tamu.edu>
2021-02-09 14:52:39 -0600

    commit from bash script

--- tensordiffeq/tensordiffeq/_build/jupyter_execute/model/compiling/index.py
+++ tensordiffeq/tensordiffeq/_build/jupyter_execute/model/compiling/index.py
@@ -9,6 +9,25 @@ that they will converge to a solution of the underlying function {cite}`pinkus19
 theoretical backing has extended into the PINN framework {cite}`shin2020convergence`. With that being said, currently the only type of network supported in
 TensorDiffEq is the fully-connected MLP network.
 
+### Layer Sizes
+
+TensorDiffEq uses the [Keras API](https://keras.io/) for neural network construction. All you need to do is define a list of layer
+sizes for your neural network. So, for a network with an `[x,t]` input, 4 layers deep, with 128 nodes, one would define
+a layer size list of `[2,128,128,1]`.
+
+For our problem we have been building in the previous sections, we can define layer sizes as such:
+
+```{code} python
+layer_sizes = [2, 128, 128, 128, 128, 1]
+```
+
+Or, if your problem is a function of `[x,y,t]`, the you could define the exact same network with a input layer with 3 nodes, i.e.
+
+```{code} python
+layer_sizes = [3, 128, 128, 128, 128, 1]
+```
+
+### Compile the Model
 
 ```{bibliography} ../../references.bib
 :style: unsrt
