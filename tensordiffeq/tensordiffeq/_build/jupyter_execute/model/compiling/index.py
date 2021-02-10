eeaff355527653b58801144dc14bac1064021a80
Levi McClenny <levimcclenny@tamu.edu>
2021-02-09 20:12:23 -0600

    commit from bash script

--- tensordiffeq/tensordiffeq/_build/jupyter_execute/model/compiling/index.py
+++ tensordiffeq/tensordiffeq/_build/jupyter_execute/model/compiling/index.py
@@ -3,13 +3,13 @@
 Once you've defined your problem, it must be compiled such that TensorDiffEq can build the loss function described
 by the boundary conditions, initial conditions, and physics defined in the previous sections.
 
-Additionally, here is where we will define the neural network size and depth. Currently, most PINN approaches use dense fully connected
+### Layer Sizes
+
+Here is where we will define the neural network size and depth. Currently, most PINN approaches use dense fully connected
 neural networks for function approximation. Fully-connected Neural Networks have some level of theoretical backing
 that they will converge to a solution of the underlying function {cite}`pinkus1999approximation,chen1993approximations`, and this
 theoretical backing has extended into the PINN framework {cite}`shin2020convergence`. With that being said, currently the only type of network supported in
 TensorDiffEq is the fully-connected MLP network.
-
-### Layer Sizes
 
 TensorDiffEq uses the [Keras API](https://keras.io/) for neural network construction. All you need to do is define a list of layer
 sizes for your neural network. So, for a network with an `[x,t]` input, 4 layers deep, with 128 nodes, one would define
@@ -31,6 +31,29 @@ layer_sizes = [3, 128, 128, 128, 128, 1]
 
 In order to compile the model, we first initialize the model we are interested in. Currently, forward solutions of PINNs are performed by
 the `CollocationSolverND()` method.
+
+#### Collocation Solver
+
+The primary method of solving forward problems in TensorDiffEq is the collocation solver. This methodology identifies points
+in the domain of the problem and collocates them to the solution via a loss function. Therefore, this is a natural application
+for a neural network function approximation.
+
+##### Instantiate the Model
+
+The `CollocationModelND()` solver can be initialized in the following way:
+
+```{code} python
+CollocationSolverND(assimilate=False)
+```
+
+Args:
+- `assimilate` a `bool` that describes whether the `CollocationModelND` will be used for data assimilation
+
+Note that very little in the solver is truly initialized, most comes later in the `compile` call.
+
+##### Compile the Model
+
+
 
 
 
