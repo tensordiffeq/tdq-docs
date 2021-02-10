dd8ca205f484806ad259e922a3f39ca00b78e20e
Levi McClenny <levimcclenny@tamu.edu>
2021-02-10 15:21:44 -0600

    commit from bash script

--- tensordiffeq/tensordiffeq/_build/jupyter_execute/model/compiling-example/index.py
+++ tensordiffeq/tensordiffeq/_build/jupyter_execute/model/compiling-example/index.py
@@ -75,7 +75,8 @@ BCs = [init, upper_x, lower_x]
 
 Now we define the [physics model](../../physics/index.ipynb), wrapping the PDE in a function and returning the strong form PDE. Gradients
 can be taken with `tf.gradients` as the execution will end up being in graph-mode once the `f_model` function is
-fed into the solver.
+fed into the solver. This is one of the biggest perks of TensorDiffEq, all the translation into fast graph-mode execution is
+built into the package.
 
 ```{code} python
 def f_model(u_model, x, t):
@@ -86,6 +87,17 @@ def f_model(u_model, x, t):
     f_u = u_t + u * u_x - (0.05 / tf.constant(math.pi)) * u_xx
     return f_u
 
+```
+
+Finally, we define the layer sizes of the FC MLP network used to approximate the solution network $u(x,t)$, initialize the model
+compile and solve.
+
+```{code} python
+layer_sizes = [2, 128, 128, 128, 128, 1]
+
+model = CollocationSolverND()
+model.compile(layer_sizes, f_model, Domain, BCs)
+model.fit(tf_iter=1000, newton_iter=1000)
 ```
 
 
