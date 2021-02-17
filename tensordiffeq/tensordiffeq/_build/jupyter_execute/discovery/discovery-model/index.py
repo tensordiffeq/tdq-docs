8328cb4793fc3abb1825da1787f0f4a52f39bd4f
Levi McClenny <levimcclenny@tamu.edu>
2021-02-17 17:49:05 -0600

    commit from bash script

--- tensordiffeq/tensordiffeq/_build/jupyter_execute/discovery/discovery-model/index.py
+++ tensordiffeq/tensordiffeq/_build/jupyter_execute/discovery/discovery-model/index.py
@@ -3,7 +3,7 @@
 Next, we will assess the discovery model, where we can perform parameter estimation of values in a PDE system.
 This is commonly referred to as "inverse modeling," as opposed to
 forward modeling (or inference) where we provide all the input information to the `f_model` and solve the whole solution via a
-neural network function approximation, forcing the residual term of the `f-model` constrained output to `0.0` via an MSE loss.
+neural network function approximation, forcing the residual term of the `f_model` constrained output to `0.0` via an MSE loss.
 
 This case is almost a more natural application of neural networks, as it is a physics-constrained methodology of supervised learning. We have data output from
 a simulation or an experiment, and we seek to map parameters of a known PDE system to that data. In this case, the neural network works via a multi-faceted loss
@@ -12,5 +12,25 @@ Therefore, we also take the loss wrt the target variables (the estimated paramet
 MSE loss against the training data. The result is a training methodology that not only trains the $u(\textbf{X}, t)$ solution (or just $u(\textbf{X})$, depending on your problem), but also
 estimates parameters within the model concurrently.
 
-In this section we will dig into the `DiscoveryMode()` in TensroDiffEq. We will discuss the pertinent methods, as well as list a [few examples](../discovery-example/index.ipynb)
+In this section we will dig into the `DiscoveryMode()` in TensorDiffEq. We will discuss the pertinent methods, as well as list a [few examples](../discovery-example/index.ipynb)
 to get you off the ground in training your models.
+
+## Initialize
+
+```{code} python
+DiscoveryModel()
+```
+
+The `DiscoveryModel` class initialized without any arguments.
+
+## Methods
+
+The `DiscoveryModel` class has methods to pass in the data for the problem, as well as methods to fit and predict similar to the `CollocationModelND()` class.
+
+### Compiling the Model
+
+First we must feed in the data and fitting parameters using the `compile` method.
+
+```{code} python
+compile(layer_sizes, f_model, X, u_star, vars_, col_weights=None)
+```
