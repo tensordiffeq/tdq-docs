91114c6e53c786e6d31b3fad0251220c5d7852fe
Levi McClenny <levimcclenny@tamu.edu>
2021-02-18 10:51:12 -0600

    commit from bash script

---
+++ tensordiffeq/tensordiffeq/_build/jupyter_execute/gpu/gpu-considerations/index.py
@@ -0,0 +1,7 @@
+# GPU Considerations
+
+graph-mode L-BFGS is typically faster on a single-GPU, given sufficient model size. On a CPU, empirically,
+it has been demonstrated that eager-mode L-BFGS is actually faster than graph-mode.
+
+We are interested in
+ community feedback! If you notice something interesting here, open a PR on these docs and let us know!
